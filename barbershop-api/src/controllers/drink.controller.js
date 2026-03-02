a2eac33ea406865111b7a66a9e92844413ffd452
Ryan Gonçalves <narsie454@gmail.com>
2026-03-02 19:13:32 -0300

    fix: refactor drink controller to use prisma

--- barbershop-api/src/controllers/drink.controller.js
+++ barbershop-api/src/controllers/drink.controller.js
@@ -1,34 +1,46 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
-const { Drink } = require('../models');
+const prisma = require('../client');
 const ApiError = require('../utils/ApiError');
 
 const createDrink = catchAsync(async (req, res) => {
-    const drink = await Drink.create(req.body);
+    const drink = await prisma.drink.create({
+        data: req.body
+    });
     res.status(httpStatus.CREATED).send(drink);
 });
 
 const getDrinks = catchAsync(async (req, res) => {
-    const drinks = await Drink.find({ active: true });
+    const drinks = await prisma.drink.findMany({
+        where: { active: true }
+    });
     res.send(drinks);
 });
 
 const updateDrink = catchAsync(async (req, res) => {
-    const drink = await Drink.findById(req.params.drinkId);
+    const drink = await prisma.drink.findUnique({
+        where: { id: req.params.drinkId }
+    });
     if (!drink) {
         throw new ApiError(httpStatus.NOT_FOUND, 'Drink not found');
     }
-    Object.assign(drink, req.body);
-    await drink.save();
-    res.send(drink);
+    const updatedDrink = await prisma.drink.update({
+        where: { id: req.params.drinkId },
+        data: req.body
+    });
+    res.send(updatedDrink);
 });
 
 const deleteDrink = catchAsync(async (req, res) => {
-    const drink = await Drink.findById(req.params.drinkId);
+    const drink = await prisma.drink.findUnique({
+        where: { id: req.params.drinkId }
+    });
     if (!drink) {
         throw new ApiError(httpStatus.NOT_FOUND, 'Drink not found');
     }
-    await drink.remove();
+    await prisma.drink.delete({
+        where: { id: req.params.drinkId }
+    });
     res.status(httpStatus.NO_CONTENT).send();
 });
 
