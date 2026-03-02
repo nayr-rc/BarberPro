cc1a866dd6af67e73e366cf9355c0af54f861c9b
Ryan Gonçalves <narsie454@gmail.com>
2026-03-02 18:49:27 -0300

    fix: reuse prisma client in serverless

--- barbershop-api/src/client.js
+++ barbershop-api/src/client.js
@@ -1,5 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+let prisma;
+
+if (!global.prisma) {
+    global.prisma = new PrismaClient();
+}
+prisma = global.prisma;
 
 module.exports = prisma;
